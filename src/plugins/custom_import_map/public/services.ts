60a0f832ba40f49329d5055621bbf78ad0346c99
Shivam Dhar <dhshivam@amazon.com>
2022-06-15 09:14:06 -0700

    fixes failing test, adds license header
    Signed-off-by: Shivam Dhar <dhshivam@amazon.com>

--- src/plugins/custom_import_map/public/services.ts
+++ src/plugins/custom_import_map/public/services.ts
@@ -3,7 +3,9 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-export const postGeojson = async (requestBody: any, http: any) => {
+import { CoreStart } from "../../../src/core/public";
+
+export const postGeojson = async (requestBody: any, http: CoreStart['http']) => {
   try {
     const response = await http.post('../api/custom_import_map/_upload', {
       body: requestBody,
@@ -14,7 +16,7 @@ export const postGeojson = async (requestBody: any, http: any) => {
   }
 };
 
-export const getIndex = async (indexName: string, http: any) => {
+export const getIndex = async (indexName: string, http: CoreStart['http']) => {
   try {
     const response = await http.post('../api/custom_import_map/_indices', {
       body: JSON.stringify({
