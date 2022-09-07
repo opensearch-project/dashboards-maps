aa7a551ffc7bbd59a4c13503bb0f96929d138b03
Shivam Dhar <dhshivam@amazon.com>
2022-09-07 11:14:59 -0700

    adds commit from 2.2 into 2.x (#42)
    Signed-off-by: Shivam Dhar <dhshivam@amazon.com>

--- src/plugins/custom_import_map/public/plugin.tsx
+++ src/plugins/custom_import_map/public/plugin.tsx
@@ -13,6 +13,7 @@ import {
 } from './types';
 import { RegionMapVisualizationDependencies } from '../../../src/plugins/region_map/public';
 import { VectorUploadOptions } from './components/vector_upload_options';
+import { OpenSearchDashboardsContextProvider } from '../../../src/plugins/opensearch_dashboards_react/public';
 
 export class CustomImportMapPlugin
   implements Plugin<CustomImportMapPluginSetup, CustomImportMapPluginStart> {
@@ -20,13 +21,21 @@ export class CustomImportMapPlugin
     core: CoreSetup,
     { regionMap }: AppPluginSetupDependencies
   ): CustomImportMapPluginSetup {
-    regionMap.addOptionTab({
-      name: 'controls',
-      title: i18n.translate('regionMap.mapVis.regionMapEditorConfig.controlTabs.controlsTitle', {
-        defaultMessage: 'Import Vector Map',
-      }),
-      editor: (props: RegionMapVisualizationDependencies) => <VectorUploadOptions {...props} />,
-    });
+    const customSetup = async () => {
+      const [coreStart] = await core.getStartServices();
+      regionMap.addOptionTab({
+        name: 'controls',
+        title: i18n.translate('regionMap.mapVis.regionMapEditorConfig.controlTabs.controlsTitle', {
+          defaultMessage: 'Import Vector Map',
+        }),
+        editor: (props: RegionMapVisualizationDependencies) => (
+          <OpenSearchDashboardsContextProvider services={coreStart}>
+            <VectorUploadOptions {...props} />
+          </OpenSearchDashboardsContextProvider>
+        ),
+      });
+    };
+    customSetup();
 
     // Return methods that should be available to other plugins
     return {};
