31b775e5d04dfaa5d8d3bf56ae3ef38e75352c43
Heemin Kim <heemin@amazon.com>
2022-11-28 18:47:21 +0000

    Enable cypress test (#93)
    Signed-off-by: Heemin Kim <heemin@amazon.com>
    
    Signed-off-by: Heemin Kim <heemin@amazon.com>

---
+++ custom_import_map/cypress/utils/constants.js
@@ -0,0 +1,9 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * The OpenSearch Contributors require contributions made to
+ * this file be licensed under the Apache-2.0 license or a
+ * compatible open source license.
+ */
+
+export const BASE_PATH = Cypress.env('base_url');
