99c3529fca54da17b3d16df69de8fee9746401e5
raintygao <tygao@amazon.com>
2022-12-21 02:20:03 +0000

    feat: add dummy config for custom map icon
    Signed-off-by: raintygao <tygao@amazon.com>

--- maps_dashboards/public/utils/getIntialLayerConfig.ts
+++ maps_dashboards/public/utils/getIntialLayerConfig.ts
@@ -7,7 +7,9 @@ import { v4 as uuidv4 } from 'uuid';
 import {
   DOCUMENTS,
   DOCUMENTS_DEFAULT_MARKER_SIZE,
-  DOCUMENTS_DEFAULT_REQUEST_NUMBER, DOCUMENTS_DEFAULT_SHOW_TOOLTIPS, DOCUMENTS_DEFAULT_TOOLTIPS,
+  DOCUMENTS_DEFAULT_REQUEST_NUMBER,
+  DOCUMENTS_DEFAULT_SHOW_TOOLTIPS,
+  DOCUMENTS_DEFAULT_TOOLTIPS,
   LAYER_VISIBILITY,
   MAP_DATA_LAYER_DEFAULT_OPACITY,
   MAP_DEFAULT_MAX_ZOOM,
@@ -17,6 +19,7 @@ import {
   MAP_VECTOR_TILE_BASIC_STYLE,
   MAP_VECTOR_TILE_DATA_SOURCE,
   OPENSEARCH_MAP_LAYER,
+  CUSTOM_MAP,
 } from '../../common';
 
 export const getLayerConfigMap = () => ({
@@ -39,6 +42,29 @@ export const getLayerConfigMap = () => ({
     name: DOCUMENTS.name,
     description: '',
     type: DOCUMENTS.type,
+    id: uuidv4(),
+    zoomRange: [MAP_DEFAULT_MIN_ZOOM, MAP_DEFAULT_MAX_ZOOM],
+    opacity: MAP_DATA_LAYER_DEFAULT_OPACITY,
+    visibility: LAYER_VISIBILITY.VISIBLE,
+    source: {
+      indexPatternRefName: undefined,
+      geoFieldType: undefined,
+      geoFieldName: undefined,
+      documentRequestNumber: DOCUMENTS_DEFAULT_REQUEST_NUMBER,
+      tooltipFields: DOCUMENTS_DEFAULT_TOOLTIPS,
+      showTooltips: DOCUMENTS_DEFAULT_SHOW_TOOLTIPS,
+    },
+    style: {
+      ...getStyleColor(),
+      borderThickness: MAP_LAYER_DEFAULT_BORDER_THICKNESS,
+      markerSize: DOCUMENTS_DEFAULT_MARKER_SIZE,
+    },
+  },
+  //TODO: update custom layer config
+  [CUSTOM_MAP.type]: {
+    name: CUSTOM_MAP.name,
+    description: '',
+    type: CUSTOM_MAP.type,
     id: uuidv4(),
     zoomRange: [MAP_DEFAULT_MIN_ZOOM, MAP_DEFAULT_MAX_ZOOM],
     opacity: MAP_DATA_LAYER_DEFAULT_OPACITY,
