b9d4b6d4f6c50bd9dc65ca36577a1478fea6a4b0
Junqiu Lei <junqiu@amazon.com>
2023-01-13 16:20:04 -0800

    2.5 Add OpenSearch Dashboards context to component and common folder changes
    Signed-off-by: Junqiu Lei <junqiu@amazon.com>

--- public/plugin.tsx
+++ public/plugin.tsx
@@ -8,7 +8,6 @@ import {
   AppMountParameters,
   CoreSetup,
   CoreStart,
-  DEFAULT_APP_CATEGORIES,
   Plugin,
   PluginInitializerContext,
 } from '../../../src/core/public';
@@ -24,10 +23,10 @@ import {
   PLUGIN_NAVIGATION_BAR_TILE,
 } from '../common/constants/shared';
 import { ConfigSchema } from '../common/config';
-
 import { AppPluginSetupDependencies } from './types';
 import { RegionMapVisualizationDependencies } from '../../../src/plugins/region_map/public';
 import { VectorUploadOptions } from './components/vector_upload_options';
+import { OpenSearchDashboardsContextProvider } from '../../../src/plugins/opensearch_dashboards_react/public';
 
 export class CustomImportMapPlugin
   implements Plugin<CustomImportMapPluginSetup, CustomImportMapPluginStart> {
@@ -80,13 +79,21 @@ export class CustomImportMapPlugin
       },
     });
 
-    regionMap.addOptionTab({
-      name: 'controls',
-      title: i18n.translate('regionMap.mapVis.regionMapEditorConfig.controlTabs.controlsTitle', {
-        defaultMessage: 'Import Vector Map',
-      }),
-      editor: (props: RegionMapVisualizationDependencies) => <VectorUploadOptions {...props} />,
-    });
+    const customSetup = async () => {
+      const [coreStart] = await core.getStartServices();
+      regionMap.addOptionTab({
+        name: 'controls',
+        title: i18n.translate('regionMap.mapVis.regionMapEditorConfig.controlTabs.controlsTitle', {
+          defaultMessage: 'Import Vector Map',
+        }),
+        editor: (props: RegionMapVisualizationDependencies) => (
+          <OpenSearchDashboardsContextProvider services={coreStart}>
+            <VectorUploadOptions {...props} />
+          </OpenSearchDashboardsContextProvider>
+        ),
+      });
+    };
+    customSetup();
 
     // Return methods that should be available to other plugins
     return {
