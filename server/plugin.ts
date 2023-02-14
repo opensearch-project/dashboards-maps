816ce7945567ec1aeec7b74a2f30599e95969410
Naveen Tatikonda <navtat@amazon.com>
2023-02-14 12:14:44 -0600

    Add maps saved object for sample datasets (#240)
    * Add Default Saved object for Maps
    
    Signed-off-by: Naveen Tatikonda <navtat@amazon.com>
    
    * Consume config and output result
    
    Signed-off-by: Kawika Avilla <kavilla414@gmail.com>
    
    * Refactor the code
    
    Signed-off-by: Naveen Tatikonda <navtat@amazon.com>
    
    * Address Review Comments
    
    Signed-off-by: Naveen Tatikonda <navtat@amazon.com>
    
    ---------
    
    Signed-off-by: Naveen Tatikonda <navtat@amazon.com>
    Signed-off-by: Kawika Avilla <kavilla414@gmail.com>
    Co-authored-by: Kawika Avilla <kavilla414@gmail.com>

--- server/plugin.ts
+++ server/plugin.ts
@@ -12,27 +12,44 @@ import {
   Logger,
 } from '../../../src/core/server';
 
-import { CustomImportMapPluginSetup, CustomImportMapPluginStart } from './types';
+import { HomeServerPluginSetup } from '../../../src/plugins/home/server';
+import { getFlightsSavedObjects } from './services/sample_data/flights_saved_objects';
+
+import {
+  AppPluginSetupDependencies,
+  CustomImportMapPluginSetup,
+  CustomImportMapPluginStart,
+} from './types';
 import { createGeospatialCluster } from './clusters';
 import { GeospatialService, OpensearchService } from './services';
 import { geospatial, opensearch } from '../server/routes';
 import { mapSavedObjectsType } from './saved_objects';
 import { capabilitiesProvider } from './saved_objects/capabilities_provider';
+import { ConfigSchema } from '../common/config';
 
 export class CustomImportMapPlugin
   implements Plugin<CustomImportMapPluginSetup, CustomImportMapPluginStart> {
   private readonly logger: Logger;
   private readonly globalConfig$;
+  private readonly config$;
 
   constructor(initializerContext: PluginInitializerContext) {
     this.logger = initializerContext.logger.get();
     this.globalConfig$ = initializerContext.config.legacy.globalConfig$;
+    this.config$ = initializerContext.config.create<ConfigSchema>();
   }
 
-  public async setup(core: CoreSetup) {
+  // Adds dashboards-maps saved objects to existing sample datasets using home plugin
+  private addMapsSavedObjects(home: HomeServerPluginSetup, config: ConfigSchema) {
+    home.sampleData.addSavedObjectsToSampleDataset('flights', getFlightsSavedObjects(config));
+  }
+
+  public async setup(core: CoreSetup, plugins: AppPluginSetupDependencies) {
     this.logger.debug('customImportMap: Setup');
     // @ts-ignore
     const globalConfig = await this.globalConfig$.pipe(first()).toPromise();
+    // @ts-ignore
+    const config = (await this.config$.pipe(first()).toPromise()) as ConfigSchema;
 
     const geospatialClient = createGeospatialCluster(core, globalConfig);
     // Initialize services
@@ -40,6 +57,8 @@ export class CustomImportMapPlugin
     const opensearchService = new OpensearchService(geospatialClient);
 
     const router = core.http.createRouter();
+    const { home } = plugins;
+
     // Register server side APIs
     geospatial(geospatialService, router);
     opensearch(opensearchService, router);
@@ -49,6 +68,8 @@ export class CustomImportMapPlugin
 
     // Register capabilities
     core.capabilities.registerProvider(capabilitiesProvider);
+
+    if (home) this.addMapsSavedObjects(home, config);
 
     return {};
   }
