881b192ac40170f82d08b55ad7ca91a1d0b94015
Ryan Bogan <rbogan@amazon.com>
2024-01-16 12:42:13 -0800

    Disable dev shm usage in cypress index.js for Chrome browsers
    Signed-off-by: Ryan Bogan <rbogan@amazon.com>

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -24,6 +24,13 @@
  * @type {Cypress.PluginConfig}
  */
 module.exports = (on, config) => {
+  on('before:browser:launch', (browser = {}, launchOptions) => {
+    if (browser.family === 'chromium') {
+      launchOptions.args.push('--disable-dev-shm-usage');
+    }
+
+    return launchOptions;
+  });
   // `on` is used to hook into various events Cypress emits
   // `config` is the resolved Cypress config
 };
