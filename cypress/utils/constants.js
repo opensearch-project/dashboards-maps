57af177b25bf5c814089f6e44f2da45bb5ec5f48
Junqiu Lei <junqiu@amazon.com>
2024-08-30 10:13:27 -0700

    Use functional test repo to run maps integration test workflow (#664)
    Signed-off-by: Junqiu Lei <junqiu@amazon.com>
    
    (cherry picked from commit 7ecc08431371a67e728315be9a5edb7240054283)
    Signed-off-by: Junqiu Lei <junqiu@amazon.com>

--- cypress/utils/constants.js
+++
@@ -1,9 +0,0 @@
-/*
- * SPDX-License-Identifier: Apache-2.0
- *
- * The OpenSearch Contributors require contributions made to
- * this file be licensed under the Apache-2.0 license or a
- * compatible open source license.
- */
-
-export const BASE_PATH = Cypress.env('base_url');
